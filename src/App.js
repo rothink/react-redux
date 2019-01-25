6c31fe51d7e3779640241e5baeeb98961cf9c7fd
rothink <rodrigoluz@live.com>
2019-01-25 01:22:39 -0200

    hello word just react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.handleClickBtn = this.handleClickBtn.bind(this)
+    }
+
+    componentWillMount() {
+        this.setState({
+            msg: ''
+        })
+    }
+
+    handleClickBtn() {
+        this.setState({
+            msg: 'Hello Word'
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClickBtn}> click to hello</button>
+                <p>{this.state.msg}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
